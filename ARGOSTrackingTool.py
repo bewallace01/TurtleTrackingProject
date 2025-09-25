f47685033435fb18fba28457161e2058b28b50ee
bewallace01 <wallacebailey32@gmail.com>
2025-09-25 17:35:13 -0400

    adding for loop and task 5

--- ARGOSTrackingTool.py
+++ ARGOSTrackingTool.py
@@ -5,7 +5,7 @@
 #   the user to view the location of the turtle for a specified
 #   date entered via user input.
 #
-# Author: John Fay (john.fay@duke.edu)
+# Author: Bailey Wallace (bailey.wallace@duke.edu)
 # Date:   Fall 2025
 #--------------------------------------------------------------
 
@@ -15,31 +15,35 @@ file_name = './data/raw/sara.txt'
 #Create a file object from the file
 file_object = open(file_name,'r')
 
-#Read contents of file into a list one line at a time
-lineString = file_object.readline()
+#Read contents of file into a list
+line_list = file_object.readlines()
+
+#Close the file
+file_object.close()
+
+#Initialize dictionaries
+date_dict = {}
+location_dict = {}
 
 #Pretend we read one line of data from the file
-while lineString:
-        # Check if line is a data line
+for lineString in line_list:
+    # Check if line is a data line
     if lineString[0] in ("#","u"):
-        lineString = file_object.readline()
         continue
 
-#Split the string into a list of data items
+    #Split the string into a list of data items
     lineData = lineString.split()
 
-#Extract items in list into variables
+    #Extract items in list into variables
     record_id = lineData[0]
     obs_date = lineData[2]
     obs_lc = lineData[4]
     obs_lat = lineData[6]
     obs_lon = lineData[7]
 
+    #Add items to dictionaries
+    date_dict[record_id] = obs_date
+    location_dict[record_id] = (obs_lat, obs_lon)
+    
     #Print the location of sara
-    print(f"Record {record_id} indicates Sara was seen at lat:{obs_lat},lon:{obs_lon} on {obs_date}")
-
-        # Move to the next line
-    lineString = file_object.readline()
-  
-# Close the file
-file_object.close()
+    #print(f"Record {record_id} indicates Sara was seen at lat:{obs_lat},lon:{obs_lon} on {obs_date}")
